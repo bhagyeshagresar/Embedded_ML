35cf25f965e36f2450d0ed2c917d1eb3f6626e00
bhagyeshagresar <bhagyeshagresar@gmail.com>
2023-10-15 22:59:26 -0500

    redid the hello world over uart

---
+++ src/vision/include/vision.hpp
@@ -0,0 +1,24 @@
+#ifndef VISION_LIB_HPP
+#define VISION_LIB_HPP
+
+
+
+namespace VisionLib{
+
+    // \include all functions for image processing, filtering, etc.
+    class VisionClass
+    {
+        
+    }
+
+
+
+
+
+
+
+
+}
+
+
+#endif 
